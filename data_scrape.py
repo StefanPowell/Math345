bcddd093693284a3c42298f5b2cd278961e634cb
StefanPowell <Stefan.Powell001@umb.edu>
2019-11-14 16:46:56 -0500

    file

--- data_scrape.py
+++ data_scrape.py
@@ -1,5 +1,6 @@
 import csv
 import urllib.request
+import re
 
 keep_first_parse = []
 
@@ -12,12 +13,27 @@ def web_partition(start, end, code):
                     break
     return keep_first_parse
 
+def clean_data(array_data, partition):
+    for y in range(len(array_data)):
+        partition = partition.replace(array_data[y], '')
+    return partition
+
+
 full_link = "https://www.worldathletics.org/records/toplists/sprints/100-metres/outdoor/men/senior/2001?regionType=world&timing=electronic&windReading=regular&page=1&bestResultsOnly=true"
 
 data = urllib.request.urlopen(full_link)
 mybytes = data.read()
 webpage_code = mybytes.decode("utf8")
 data.close()
-
 first_partition = ''.join(web_partition('<div class="toplists__standard">', '<div class="container container--pagination toplist-pagination">', webpage_code));
-print(first_partition)
+
+
+strip_data = first_partition
+remove_array = ['</a>', '">', '<tr>', '<th>', '</th>', '<thead>', '</tr>', '</thead>', '<tbody>', '<td data-th="', '</td>', '">'
+            '                                        ', '                                        ', '                                ',
+                '    ', ' ']
+strip_data = clean_data(remove_array, first_partition);
+strip_data = re.sub('<div class=.*?Results Score', '', strip_data, flags=re.DOTALL)
+strip_data = strip_data.split("\n")
+strip_data = list(filter(None, strip_data))
+print(strip_data)
